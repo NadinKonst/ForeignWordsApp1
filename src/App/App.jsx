13037d8b99fd107f68a6790c9a2a15ec62e9799f
NadinKonst <nadikon@gmail.com>
2024-08-05 20:06:18 +0700

    add loader

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,22 +7,24 @@ import { words } from "../dataWords";
 import Menu from "../сomponents/Menu/Menu";
 import Page404 from "../сomponents/Page404/Page404";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loader from "../сomponents/Loader/Loader";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <div className="wrapper">
-    <Menu/>
-    <div className="header-content">
-        <Header />
-    </div>
-    <Routes>
-      <Route path="/" element={<WordList words={words} />}/>
-      <Route path="slider" element={<WordCardSlider words={words} />}/>
-      <Route path="*" element={<Page404 />}/>
-      </Routes>
-      <Footer />
-    </div>
+      <div className="wrapper">
+        <Menu />
+        <div className="header-content">
+          <Header />
+        </div>
+        <Routes>
+          <Route path="/" element={<WordList words={words} />} />
+          <Route path="slider" element={<WordCardSlider words={words} />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+        <Footer />
+        <Loader />
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/сomponents/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import "./Loader.scss";
+
+export default function Loader() {
+  return (
+    <div>
+      <div id="man">
+        <div id="eye-l"></div>
+        <div id="eye-r"></div>
+        <div id="nose"></div>
+        <div id="mouth"></div>
+      </div>
+      <div id="wall"></div>
+    </div>
+  );
+}
