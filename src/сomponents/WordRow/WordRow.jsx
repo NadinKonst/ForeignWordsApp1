b992bd523e2b2f1f89cd77310308399650b19ef2
NadinKonst <nadikon@gmail.com>
2024-08-11 20:46:13 +0700

    add changes

--- src/сomponents/WordRow/WordRow.jsx
+++ src/сomponents/WordRow/WordRow.jsx
@@ -1,135 +1,10 @@
-// import "./WordRow.scss";
-// import { useState } from "react";
-// import { validateFields } from "./utils";
-
-// const WordRow = ({ word, index, onSave }) => {
-//   const [editMode, setEditMode] = useState(false);
-//   const [editedWord, setEditedWord] = useState({ ...word });
-//   const [errors, setErrors] = useState({});
-
-//   const handleEditWord = () => {
-//     setEditMode(true);
-//     setErrors({});
-//   };
-
-//   const handleCancel = () => {
-//     setEditMode(false);
-//     setEditedWord({ ...word });
-//     setErrors({});
-//   };
-
-//   const handleInputChange = (fieldName, value) => {
-//     setEditedWord((prevEditedWord) => ({
-//       ...prevEditedWord,
-//       [fieldName]: value,
-//     }));
-//     setErrors({});
-//   };
-
-//   const handleSave = () => {
-//     const newErrors = validateFields(editedWord);
-
-//     if (Object.keys(newErrors).length > 0) {
-//       setErrors(newErrors);
-//       alert("Error: Unable to save data.");
-//       return;
-//     }
-
-//     console.log("Saved data:", editedWord);
-
-//     onSave(index, editedWord);
-//     setEditMode(false);
-//     setErrors({});
-//   };
-
-//   return (
-//     <tr key={index}>
-//       <td>
-//         {editMode ? (
-//           <>
-//             <input
-//               type="text"
-//               value={editedWord.english}
-//               className={`input-word ${errors.english ? "error" : ""}`}
-//               onChange={(e) => handleInputChange("english", e.target.value)}
-//             />
-//             {errors.english && (
-//               <span className="error-message">{errors.english}</span>
-//             )}
-//           </>
-//         ) : (
-//           word.english
-//         )}
-//       </td>
-//       <td>
-//         {editMode ? (
-//           <>
-//             <input
-//               type="text"
-//               value={editedWord.transcription}
-//               className={`input-word ${errors.transcription ? "error" : ""}`}
-//               onChange={(e) =>
-//                 handleInputChange("transcription", e.target.value)
-//               }
-//             />
-//             {errors.transcription && (
-//               <span className="error-message">{errors.transcription}</span>
-//             )}
-//           </>
-//         ) : (
-//           word.transcription
-//         )}
-//       </td>
-//       <td>
-//         {editMode ? (
-//           <>
-//             <input
-//               type="text"
-//               value={editedWord.russian}
-//               className={`input-word ${errors.russian ? "error" : ""}`}
-//               onChange={(e) => handleInputChange("russian", e.target.value)}
-//             />
-//             {errors.russian && (
-//               <span className="error-message">{errors.russian}</span>
-//             )}
-//           </>
-//         ) : (
-//           word.russian
-//         )}
-//       </td>
-//       <td className="actions">
-//         {editMode ? (
-//           <>
-//             <button type="button" onClick={handleSave}>
-//               Save
-//             </button>
-//             <button type="button" onClick={handleCancel}>
-//               Cancel
-//             </button>
-//           </>
-//         ) : (
-//           <>
-//             <button type="button" onClick={handleEditWord}>
-//               Edit
-//             </button>
-//             <button type="button">Delete</button>
-//           </>
-//         )}
-//       </td>
-//     </tr>
-//   );
-// };
-
-// export default WordRow;
-
 import "./WordRow.scss";
 import { useState, useContext } from "react";
-import { validateFields } from "./utils";
 import { WordContext } from "../MyContext/WordContext";
+import { validateFields } from "./utils";
 
-const WordRow = ({ word, index, onSave, onDelete }) => {
-  const { words, loading, error, fetchWords, addWord, updateWord, deleteWord } =
-    useContext(WordContext);
+const WordRow = ({ word, onSave }) => {
+  const { deleteWord } = useContext(WordContext);
   const [editMode, setEditMode] = useState(false);
   const [editedWord, setEditedWord] = useState({ ...word });
   const [errors, setErrors] = useState({});
@@ -153,24 +28,24 @@ const WordRow = ({ word, index, onSave, onDelete }) => {
     setErrors({});
   };
 
-  const handleSave = async () => {
+  const handleSave = () => {
     const newErrors = validateFields(editedWord);
+
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
       alert("Error: Unable to save data.");
       return;
     }
 
-    try {
-      await updateWord(word.id, editedWord);
+    console.log("Saved data:", editedWord);
+    onSave(editedWord);
+    setEditMode(false);
+    setErrors({});
+  };
 
-      console.log("Saved data:", editedWord);
-
-      onSave(editedWord);
-      setEditMode(false);
-      setErrors({});
-    } catch (error) {
-      alert("Error: " + error.message);
+  const handleDelete = () => {
+    if (window.confirm("Are you sure you want to delete this word?")) {
+      deleteWord(word.id);
     }
   };
 
@@ -178,47 +53,55 @@ const WordRow = ({ word, index, onSave, onDelete }) => {
     <tr>
       <td>
         {editMode ? (
-          <input
-            type="text"
-            value={editedWord.english}
-            className={'input-word ${errors.english ? "error" : ""}'}
-            onChange={(e) => handleInputChange("english", e.target.value)}
-          />
+          <>
+            <input
+              type="text"
+              value={editedWord.english}
+              className={`input-word ${errors.english ? "error" : ""}`}
+              onChange={(e) => handleInputChange("english", e.target.value)}
+            />
+            {errors.english && (
+              <span className="error-message">{errors.english}</span>
+            )}
+          </>
         ) : (
           word.english
         )}
-        {errors.english && (
-          <span className="error-message">{errors.english}</span>
-        )}
       </td>
       <td>
         {editMode ? (
-          <input
-            type="text"
-            value={editedWord.transcription}
-            className={'input-word ${errors.transcription ? "error" : ""}'}
-            onChange={(e) => handleInputChange("transcription", e.target.value)}
-          />
+          <>
+            <input
+              type="text"
+              value={editedWord.transcription}
+              className={`input-word ${errors.transcription ? "error" : ""}`}
+              onChange={(e) =>
+                handleInputChange("transcription", e.target.value)
+              }
+            />
+            {errors.transcription && (
+              <span className="error-message">{errors.transcription}</span>
+            )}
+          </>
         ) : (
           word.transcription
         )}
-        {errors.transcription && (
-          <span className="error-message">{errors.transcription}</span>
-        )}
       </td>
       <td>
         {editMode ? (
-          <input
-            type="text"
-            value={editedWord.russian}
-            className={'input-word ${errors.russian ? "error" : ""}'}
-            onChange={(e) => handleInputChange("russian", e.target.value)}
-          />
+          <>
+            <input
+              type="text"
+              value={editedWord.russian}
+              className={`input-word ${errors.russian ? "error" : ""}`}
+              onChange={(e) => handleInputChange("russian", e.target.value)}
+            />
+            {errors.russian && (
+              <span className="error-message">{errors.russian}</span>
+            )}
+          </>
         ) : (
           word.russian
-        )}
-        {errors.russian && (
-          <span className="error-message">{errors.russian}</span>
         )}
       </td>
       <td className="actions">
@@ -236,7 +119,7 @@ const WordRow = ({ word, index, onSave, onDelete }) => {
             <button type="button" onClick={handleEditWord}>
               Edit
             </button>
-            <button type="button" onClick={onDelete}>
+            <button type="button" onClick={handleDelete}>
               Delete
             </button>
           </>
