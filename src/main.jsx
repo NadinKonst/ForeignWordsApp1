2f38728abfdb302c3ede60610c646664ec47183d
NadinKonst <nadikon@gmail.com>
2024-08-18 21:05:58 +0700

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App/App.jsx";
+import App from "./App/AppMobX.jsx";
 import Provider from "./context/Provider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
